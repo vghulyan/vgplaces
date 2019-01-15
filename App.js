be724477f7e4facf50d372bf12fad766fc2860a6
Vardan Ghulyan <vardanghulyan@Vardans-MacBook-Pro.local>
2019-01-15 13:08:03 +0000

    Added a dynamic image coming from internet

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {Platform, StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
-import placeImage from './src/assets/Matenadaran.jpg';
+//import placeImage from './src/assets/Matenadaran.jpg';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,7 +25,10 @@ export default class App extends Component<Props> {
         places: prevState.places.concat({
           key: Math.random(),
           name: placeName,
-          image: placeImage
+          //image: placeImage
+          image: {
+            uri: "https://lh5.googleusercontent.com/proxy/yHgJqa6ePPwafGEJ_grwalkoWaf5pYv9F5xRf5n2dBIPnI_dm9_9Ng12NoeXw79Q3PbKxsljsaKKnvZL6a5WHkMUIH4vHhiqiy9h6OycwT7Thjkj6xNlueKy5PSPEld5yKjybhrs0iNEBKzB=s1536-k-no"
+          }
         })
       }
     })
